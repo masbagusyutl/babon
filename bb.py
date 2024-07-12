2d0d1fe72462f2390d03857858b6e5f4de70e0d8
masbagusyutl <82576353+masbagusyutl@users.noreply.github.com>
2024-07-12 23:21:35 +0700

    bb.py

--- bb.py
+++ bb.py
@@ -80,25 +80,7 @@ def run_tasks_every_2_hours(params):
 
         print("Semua akun proses untuk tugas charge battery dan repair finger selesai")
         
-        two_hour_seconds = 2 * 60 * 60
-        daily_task_seconds = time_until_next_7am_wib()
-        
-        while two_hour_seconds > 0 or daily_task_seconds > 0:
-            now = datetime.now(timezone.utc).astimezone(timezone(timedelta(hours=7)))
-            formatted_two_hour = format_seconds(two_hour_seconds)
-            formatted_daily_task = format_seconds(daily_task_seconds)
-            current_time = now.strftime('%H:%M:%S WIB')
-            
-            print(f"Waktu tersisa untuk memulai ulang: {formatted_two_hour}")
-            print(f"Saat ini juga masih menunggu tugas harian pada jam 7 WIB")
-            print(f"Waktu tersisa untuk tugas harian: {formatted_daily_task}")
-            print(f"Jam saat ini: {current_time}", end='\r')
-            
-            time.sleep(1)
-            two_hour_seconds -= 1
-            daily_task_seconds -= 1
-            
-        print("\n")  # Newline after countdown is complete
+        time.sleep(2 * 60 * 60)  # Tunggu 2 jam
 
 # Fungsi untuk menjalankan tugas harian pada jam 7 WIB
 def run_daily_tasks_at_7(params):
@@ -115,7 +97,7 @@ def run_daily_tasks_at_7(params):
             claim_daily_combo_reward(base_claim_daily_combo_url + param)
         
         print("Semua akun sudah diproses untuk tugas harian. Menunggu hingga jam 7 WIB berikutnya...")
-        countdown(24 * 60 * 60)  # 24 jam dalam detik
+        time.sleep(24 * 60 * 60)  # Tunggu 24 jam
 
 # Fungsi untuk hitung mundur
 def countdown(seconds):
@@ -123,23 +105,10 @@ def countdown(seconds):
         mins, secs = divmod(seconds, 60)
         hours, mins = divmod(mins, 60)
         timeformat = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
+        print(f"Waktu tersisa: {timeformat}")
         time.sleep(1)
         seconds -= 1
     print()  # Newline after countdown is complete
-
-# Fungsi untuk menghitung waktu hingga jam 7 WIB berikutnya
-def time_until_next_7am_wib():
-    now = datetime.now(timezone.utc).astimezone(timezone(timedelta(hours=7)))  # Waktu WIB
-    next_run = now.replace(hour=7, minute=0, second=0, microsecond=0)
-    if now > next_run:
-        next_run += timedelta(days=1)
-    return (next_run - now).seconds
-
-# Fungsi untuk format detik menjadi jam:menit:detik
-def format_seconds(seconds):
-    mins, secs = divmod(seconds, 60)
-    hours, mins = divmod(mins, 60)
-    return '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs)
 
 # Main function
 if __name__ == "__main__":
